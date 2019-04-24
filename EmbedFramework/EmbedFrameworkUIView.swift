f5579bb0b03c1f4d0cb179a6fda1bbe432f9e372
Nicholas Haley <nic@ada.support>
2019-04-24 10:44:59 -0400

    First commit

---
+++ EmbedFramework/EmbedFrameworkUIView.swift
@@ -0,0 +1,47 @@
+//
+//  EmbedFrameworkUIView.swift
+//  EmbedFramework
+//
+//  Created by Nicholas Haley on 2019-04-23.
+//  Copyright © 2019 Ada Support. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+public class EmbedFrameworkUIView: UIView {
+
+    @IBOutlet weak var webView: WKWebView!
+    
+    public override init(frame: CGRect) {
+        // For use in code
+        super.init(frame: frame)
+        setUpView()
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        // For use in Interface Builder
+        super.init(coder: aDecoder)
+        setUpView()
+    }
+    
+    let nibName = "ASEmbedView"
+    var contentView: UIView!
+    
+    private func setUpView() {
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: self.nibName, bundle: bundle)
+        self.contentView = (nib.instantiate(withOwner: self, options: nil).first as! UIView)
+        addSubview(contentView)
+        
+        contentView.center = self.center
+        contentView.autoresizingMask = []
+        contentView.translatesAutoresizingMaskIntoConstraints = true
+        
+        if let htmlPath = Bundle.main.path(forResource: "index", ofType: "html") {
+            let url = URL(fileURLWithPath: htmlPath)
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
+    }
+}
