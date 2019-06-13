be3fb3ec0ee02fd08bf30b603378b45ae92cac0d
Nicholas Haley <nic@ada.support>
2019-06-13 12:58:10 -0400

    Remove commented offline view code

--- EmbedFramework/OfflineViewController.swift
+++ EmbedFramework/OfflineViewController.swift
@@ -20,9 +20,6 @@ class OfflineViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        container.layer.borderColor = UIColor.black.cgColor
-//        container.layer.borderWidth = 2.0
-//        container.layer.cornerRadius = 5.0
     }
     
 }
