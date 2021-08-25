a2ee67c75dc474c7ef32c98d59457e431e58403a
Anton Doudarev <antonthedev@gmail.com>
2021-08-25 12:32:34 -0400

    Added carthage support to Ada

---
+++ Carthage/Build/AdaEmbedFramework.xcframework/ios-arm64_armv7/AdaEmbedFramework.framework/Headers/EmbedFramework.h
@@ -0,0 +1,19 @@
+//
+//  EmbedFramework.h
+//  EmbedFramework
+//
+//  Created by Nicholas Haley on 2019-04-23.
+//  Copyright © 2019 Ada Support. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EmbedFramework.
+FOUNDATION_EXPORT double EmbedFrameworkVersionNumber;
+
+//! Project version string for EmbedFramework.
+FOUNDATION_EXPORT const unsigned char EmbedFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EmbedFramework/PublicHeader.h>
+
+
